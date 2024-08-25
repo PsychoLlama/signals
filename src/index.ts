359dc6b887177fe89f28b72264bb24a7a994be87
Jesse Gibson <JesseTheGibson@gmail.com>
2024-08-24 20:08:33 -0400

    Export `behavior` and `external` types

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ export { Signal } from 'signal-polyfill';
 
 export { action } from './action';
 export { atom, type Atom } from './atom';
-export { behavior } from './behavior';
+export { behavior, type Behavior } from './behavior';
 export { computed, type Computed } from './computed';
-export { external } from './external';
+export { external, type ExternalSource } from './external';
 export { get, type Source } from './get';
 export { swap, type Sink } from './swap';
